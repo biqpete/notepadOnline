7e3e508e64d474fb25741797c4ecbf2a99d36b36
biqpete <36991526+biqpete@users.noreply.github.com>
2018-06-26 14:21:25 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = mysqli_connect("localhost","id6066330_ochenx", "mojehaslo1", "id6066330_usernames");
+$link = mysqli_connect("localhost","id6066330_ochenx", "*****haslo*******", "id6066330_usernames");
 
 if (mysqli_connect_error()) {
 
